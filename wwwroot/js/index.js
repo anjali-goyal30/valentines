dc602ae2e3f2facc981c39aea3033fa133c6f203
Anjali Goyal <anjali.goyal@calabrio.com>
2025-02-10 01:49:29 +0530

    Initial commit

---
+++ wwwroot/js/index.js
@@ -0,0 +1,13 @@
+﻿jQuery(document).ready(function ($) {
+    $("#btnClaim").on("click", function () {
+        window.location.href = $("#claimUrl").val();
+    });
+
+    $("#claimForm").on("submit", function (event) {
+        event.preventDefault();
+        var passKey = $("#exampleInputPassword1").val();
+        if (passKey != undefined && passKey !== null && passKey === "ak472022") {
+            window.location.href = $("#submitUrl").val();
+        }
+    });
+});
